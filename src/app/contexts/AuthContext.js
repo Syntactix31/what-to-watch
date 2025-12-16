7103aa954f67155563ae60024a33d11d8ee05b4a
Levyyyy <Levi.moreau@edu.sait.ca>
2025-12-16 02:52:55 -0700

    fixing authentication

--- src/app/contexts/AuthContext.js
+++ src/app/contexts/AuthContext.js
@@ -1,3 +1,63 @@
+"use client";
+ 
+import { useContext, createContext, useState, useEffect } from "react";
+import {
+  signOut,
+  onAuthStateChanged,
+  sendSignInLinkToEmail, 
+  isSignInWithEmailLink,
+  signInWithEmailLink,
+} from "firebase/auth";
+import { auth } from "./firebase";
+ 
+const AuthContext = createContext();
 
+ 
+export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const sendEmailLink = async (email) => {
+    const actionCodeSettings = {
+      url: window.location.origin + '/auth/complete',
+      handleCodeInApp: true,
+    };
+    try {
+      await sendSignInLinkToEmail(auth, email, actionCodeSettings);
+      window.localStorage.setItem('emailForSignIn', email);
+    } catch (error) {
+      console.error('Error sending email:', error);
+      throw error;
+    }
+  };
+
+  const firebaseSignOut = () => signOut(auth);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, setUser);
+    return () => unsubscribe();
+  }, []);
+
+  useEffect(() => {
+    if (isSignInWithEmailLink(auth, window.location.href)) {
+      let email = window.localStorage.getItem('emailForSignIn');
+      if (!email) email = window.prompt('Enter email for confirmation');
+      if (email) {
+        signInWithEmailLink(auth, email, window.location.href)
+          .then(() => window.localStorage.removeItem('emailForSignIn'))
+          .catch(console.error);
+      }
+    }
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, sendEmailLink, firebaseSignOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useUserAuth = () => {
+  return useContext(AuthContext);
+};
 
 
--- src/app/layout.js
+++ src/app/layout.js
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <AuthContextProvider>{children}</AuthContextProvider>
       </body>
     </html>
   );
